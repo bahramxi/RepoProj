c33f2ff758966c9f68ff6af6aef49c000da0abb1
MostafaLenovo <bahramxi@gmail.com>
2022-12-12 14:33:33 +0330

    second commit

---
+++ DownloadMusic/Models/MusicTrackModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DownloadMusic.Models
+{
+    public class MusicTrackModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Songwriter { get; set; }
+        public string Vocalist { get; set; }
+        public string Album { get; set; }
+        public string MusicText { get; set; }
+        public IFormFile MusicFile { get; set; }
+        public IFormFile Image { get; set; }
+        public string Description { get; set; }
+    }
+}
