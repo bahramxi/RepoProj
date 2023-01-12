0722bc9eb28763fcb5b6034dd9d50c8624b70983
16220 <16220@I-52-16220>
2023-01-12 10:29:07 +0330

    add url jquery validation to layout and create downloadmusicview model for front

---
+++ DownloadMusic/Models/DownloadMusicViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DownloadMusic.Models
+{
+    public class DownloadMusicViewModel : ResponseModel
+    {
+        [HiddenInput]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "{0} را وارد نمایید")]
+        [Display(Name = "نام موزیک", Prompt = "نام موزیک"), MaxLength(150, ErrorMessage = "حداکثر {0} باید {1} کاراکتر باشد")]
+        public string TitleMusic { get; set; }
+
+
+        [Display(Name = "ترانه سرا", Prompt = "ترانه سرا"), MaxLength(150, ErrorMessage = "حداکثر {0} باید {1} کاراکتر باشد")]
+        public string Songwriter { get; set; }
+
+
+        [Required(ErrorMessage = "{0} را وارد نمایید")]
+        [Display(Name = "خواننده", Prompt = "خواننده"), MaxLength(150, ErrorMessage = "حداکثر {0} باید {1} کاراکتر باشد")]
+        public string Vocalist { get; set; }
+
+
+        [Display(Name = "نام آلبوم", Prompt = "نام آلبوم"), MaxLength(150, ErrorMessage = "حداکثر {0} باید {1} کاراکتر باشد")]
+        public string Album { get; set; }
+
+
+        [Display(Name = "دسته بندی آهنگ", Prompt = "دسته بندی آهنگ")]
+        public MusicCategory MusicCategory { get; set; }
+
+
+        [Display(Name = "متن آهنگ", Prompt = "متن آهنگ"), MaxLength(150, ErrorMessage = "حداکثر {0} باید {1} کاراکتر باشد")]
+        public string MusicText { get; set; }
+
+
+        [Display(Name = "توضیحات آهنگ", Prompt = "توضیحات آهنگ"), MaxLength(150, ErrorMessage = "حداکثر {0} باید {1} کاراکتر باشد")]
+        public string Description { get; set; }
+
+
+        [Display(Name = "متن آهنگ", Prompt = "متن آهنگ"), MaxLength(150, ErrorMessage = "حداکثر {0} باید {1} کاراکتر باشد")]
+        public string MusicFilePath { get; set; }
+
+
+        [Display(Name = "متن آهنگ", Prompt = "متن آهنگ"), MaxLength(150, ErrorMessage = "حداکثر {0} باید {1} کاراکتر باشد")]
+        public string ImageFilePath { get; set; }
+    }
+}
